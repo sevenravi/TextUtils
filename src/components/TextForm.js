961e3b0543112c783abf87cdabd77f7669c64cc8
Ravi <ravikumarmahato7@gmail.com>
2022-12-25 20:27:37 +0545

    partial

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,10 +2,17 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
 
-    const [text, setText] = useState('Enter your Text'); 
+    const [text, setText] = useState(''); 
+    const [fword, findword] = useState(''); 
+    const [rword, replaceword] = useState(''); 
 
     const handelUppercase =()=>{
         let newText=text.toUpperCase();
+        setText(newText)
+    }
+
+    const handelLowercase =()=>{
+        let newText=text.toLowerCase();
         setText(newText)
     }
 
@@ -13,15 +20,48 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
-
+    const handlefindchange = (event)=>{
+        findword(event.target.value);
+    }
+    const handlereplacechange = (event)=>{
+        replaceword(event.target.value);
+    }
+    
+    const handelReplace=()=>{
+        let newtext = text.replaceAll(fword,rword);
+        setText(newtext);
+    }
 
     return (
-        <div>
+            <>
+        <div className='container my-2'>
             <h3>{props.heading}</h3>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>                
             </div>
-            <button className="btn btn-primary" onClick={handelUppercase}> Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handelUppercase}> Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handelLowercase}> Lowercase</button>
+
+            <div className='form-floating my-3'>
+            <textarea className='form-control' value={fword} onChange={handlefindchange}></textarea>
+            <label htmlFor="form-floating">Find</label>
+
+            </div>
+            <div className="form-floating my-2">
+            <textarea className='form-control' value={rword} onChange={handlereplacechange}></textarea>
+            <label htmlFor="form-floating">Replace</label>
+            </div>
+            <button className="btn btn-primary mx-2" onClick={handelReplace}> Replace</button>
+
+            
         </div>
+        <div className="container my-2">
+            <h2>Text summary:</h2>
+            <p>{text.split(" ").length} Words, {text.length} Characters</p>
+            <h3>Preview:</h3>
+            <p>{text}</p>
+            
+        </div>
+            </>
     )
 }
